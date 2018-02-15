94ec6b8dffbb0fb5cc4b680b2aec5219b2c3ffc5
vamshi <yuvnith.vamshi@gmail.com>
2018-02-15 10:47:48 +0530

    Creating New GradeBookTypes 3,4,5,6

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+
+
+    }
+}
